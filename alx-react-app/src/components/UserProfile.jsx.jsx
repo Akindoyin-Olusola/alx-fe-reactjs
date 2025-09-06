03041db6f5e8cd78783de3b67810d62fd4962cf4
Akindoyin-Olusola <elizabetholusola2020@gmail.com>
2025-09-06 23:12:42 +0100

    New

--- alx-react-app/src/components/UserProfile.jsx.jsx
+++ alx-react-app/src/components/UserProfile.jsx.jsx
@@ -1,12 +1,11 @@
 // src/components/UserProfile.jsx
-function UserProfile(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <p>Age: {props.age}</p>
-      <p>Bio: {props.bio}</p>
-    </div>
-  )
-}
-
+const UserProfile = (props) => {
+   return (
+     <div>
+       <h2>{props.name}</h2>
+       <p>Age: {props.age}</p>
+       <p>Bio: {props.bio}</p>
+     </div>
+   );
+ };
 export default UserProfile
