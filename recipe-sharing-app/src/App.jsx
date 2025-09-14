47f3cdfa3f60aae9e05df7981eea61cd684518fa
Akindoyin-Olusola <elizabetholusola2020@gmail.com>
2025-09-14 18:45:35 +0100

    Fix: Add proper imports for RecipeList and AddRecipeForm in App.jsx

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,23 +1,14 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/Navbar";
-import Footer from "./components/Footer";
-import Home from "./components/Home";
-import About from "./components/About";
-import Services from "./components/Services";
-import Contact from "./components/Contact";
+// src/App.jsx
+import RecipeList from './components/RecipeList';
+import AddRecipeForm from './components/AddRecipeForm';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/services" element={<Services />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <div>
+      <h1>🍲 Recipe Sharing App</h1>
+      <RecipeList />
+      <AddRecipeForm />
+    </div>
   );
 }
 
