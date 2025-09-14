ba75b58f8a1157131ed675dbe19f96542979b188
Akindoyin-Olusola <elizabetholusola2020@gmail.com>
2025-09-14 19:26:51 +0100

    Implement routing in App.jsx using BrowserRouter and Routes

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,26 +1,27 @@
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
 import RecipeDetails from "./components/RecipeDetails";
 
 function App() {
   return (
-    <div>
-      <h1>🍲 Recipe Sharing App</h1>
-
-      <Routes>
-        <Route
-          path="/"
-          element={
-            <>
-              <RecipeList />
-              <AddRecipeForm />
-            </>
-          }
-        />
-        <Route path="/recipes/:id" element={<RecipeDetails />} />
-      </Routes>
-    </div>
+    <Router>
+      <div>
+        <h1>🍲 Recipe Sharing App</h1>
+        <Routes>
+          <Route
+            path="/"
+            element={
+              <>
+                <RecipeList />
+                <AddRecipeForm />
+              </>
+            }
+          />
+          <Route path="/recipe/:id" element={<RecipeDetails />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
