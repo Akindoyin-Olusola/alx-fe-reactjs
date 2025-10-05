a52e22ce9f3c82f8d5259bf0400567f585c2d686
Akindoyin-Olusola <elizabetholusola2020@gmail.com>
2025-10-05 22:06:03 +0100

    New

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -6,7 +6,7 @@ function UserProfile() {
       <img
         src="https://via.placeholder.com/150"
         alt="User"
-        className="w-32 h-32 rounded-full border-4 border-blue-500 mb-4"
+        className="sm:w-24 sm:h-24 md:w-36 md:h-36 rounded-full border-4 border-blue-500 mb-4"
       />
       <h1 className="text-lg md:text-xl font-bold text-gray-800 mb-2 text-center">John Doe</h1>
       <p className="text-sm md:text-lg text-gray-600 text-center">Developer at Example Co</p>
