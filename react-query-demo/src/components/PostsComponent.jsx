68753b6ad0583b2b7d9a88c797588cf73af3f2d8
Akindoyin-Olusola <elizabetholusola2020@gmail.com>
2025-10-12 23:33:16 +0100

    correction

--- react-query-demo/src/components/PostsComponent.jsx
+++ react-query-demo/src/components/PostsComponent.jsx
@@ -12,6 +12,7 @@ const PostsComponent = () => {
     data: posts,
     error,
     isLoading,
+    isError,
     isFetching,
     refetch,
   } = useQuery({
@@ -24,7 +25,7 @@ const PostsComponent = () => {
   });
 
   if (isLoading) return <p>Loading...</p>;
-  if (error) return <p>Error: {error.message}</p>;
+  if (isError) return <p>Error: {error.message}</p>;
 
   return (
     <div style={{ padding: "20px" }}>
